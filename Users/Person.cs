f0a26e8d665c33ca8b9f237d8882ff21fdd593d5
David <98875282+daverbk@users.noreply.github.com>
2022-02-17 11:07:40 +0300

    TASK-3_1: Added blank lines around properties in Person.cs

--- Users/Person.cs
+++ Users/Person.cs
@@ -5,7 +5,9 @@ namespace Users
     public abstract class Person
     {
         public string Name { get; set; }
+        
         public string SecondName { get; set; }
+        
         public Guid Id { get; set; }
     }
 }
