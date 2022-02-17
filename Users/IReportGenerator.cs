2f73414980c7d42965afd2af4d973fba3b46a243
David <98875282+daverbk@users.noreply.github.com>
2022-02-17 11:06:47 +0300

    TASK-3_1: Added blank lines around methods in IReportGenerator.cs

--- Users/IReportGenerator.cs
+++ Users/IReportGenerator.cs
@@ -4,7 +4,9 @@ namespace Users
     public interface IReportGenerator
     {
         void PrintLine();
+        
         void PrintRow(params string[] columns);
+        
         string AlignCentre(string text, int width);
     }
 }
