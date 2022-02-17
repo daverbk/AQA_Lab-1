c55df6e957faa23861cb7ad252eb95ae204329bc
David <98875282+daverbk@users.noreply.github.com>
2022-02-17 11:04:23 +0300

    TASK-3_1: Added blank lines around properties in Employee.cs

--- Users/Employee.cs
+++ Users/Employee.cs
@@ -5,11 +5,17 @@ namespace Users
     public class Employee : Person, IJob
     {
         public string JobTitle { get; set; }
+        
         public string JobDescription { get; set; }
+        
         public decimal JobSalary { get; set; }
+        
         public string CompanyName { get; set; }
+        
         public string CompanyCountry { get; set; }
+        
         public string CompanyCity { get; set; }
+        
         public string CompanyAddress { get; set; }
         
         public void SelfPresentation()
