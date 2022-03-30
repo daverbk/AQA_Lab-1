d2e8ce312148dcb117af72f361a454b666b7725e
David Ryabko <davidryabko13@gmail.com>
2022-03-30 10:47:41 +0300

    TASK-10: Restructured methods

--- NUnitTestPractice/NUnitTestPractice/CalculatorTest.cs
+++ NUnitTestPractice/NUnitTestPractice/CalculatorTest.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using NUnit.Framework;
 
 namespace NUnitTestPractice
@@ -15,13 +14,13 @@ namespace NUnitTestPractice
         {
             Console.WriteLine("Setting it up...");
         }
-        
+
         [TearDown]
         public void TearDown()
         {
             Console.WriteLine("Tearing it down...");
         }
-        
+
         [OneTimeSetUp]
         public void OneTimeSetup()
         {
@@ -34,7 +33,7 @@ namespace NUnitTestPractice
         [TestCase(-5, -3, -2)]
         [TestCase(1, -1, 2)]
         [TestCase(0, 0, 0)]
-        public void TestSum(int result, int num1, int num2)
+        public void testSum(int result, int num1, int num2)
         {
             var expected = result;
             var actual = _calculator.Sum(num1, num2);
@@ -48,24 +47,24 @@ namespace NUnitTestPractice
         [TestCase(-2, 4, -2)]
         [TestCase(-2, 6, -3)]
         [TestCase(2, 7, 3)]
-        public void TestDiv_Int(int result, int num1, int num2)
+        public void testDiv_Int(int result, int num1, int num2)
         {
             var expected = result;
             var actual = _calculator.Div(num1, num2);
 
             Assert.AreEqual(expected, actual);
         }
-        
-        [TestCaseSource(typeof(MyDataClass), nameof(MyDataClass.TestCases))]
+
+        [TestCaseSource(typeof(Data), nameof(Data.TestCases))]
         [Ignore("Ignoring to use all of the mentioned during lecture attributes :)")]
-        public int TestDiv_WithCaseSource(int n, int d)
+        public int testDiv_WithCaseSource(int n, int d)
         {
             return _calculator.Div(n, d);
         }
 
         [Property("Priority", 5)]
         [Test]
-        public void TestDivByZero_Int()
+        public void When_DividedByZero_ThrowsException()
         {
             Assert.Throws<DivideByZeroException>(delegate { _calculator.Div(0, 0); });
         }
@@ -82,60 +81,47 @@ namespace NUnitTestPractice
 
             Assert.AreEqual(expected, actual);
         }
-        
+
         [Property("Priority", 0)]
         [Test]
         public void TestsForTheSakeOfAssertionsPractice()
         {
             Calculator calc;
             calc = _calculator;
-            
+
             Assert.Multiple(() =>
             {
                 double a = TestContext.CurrentContext.Random.NextDouble(100);
                 double b = TestContext.CurrentContext.Random.NextDouble(10, 20);
-    
+
                 TestContext.Out.WriteLineAsync("First Double is " + a);
                 TestContext.Out.WriteLineAsync($"Second Double is {b}");
-    
+
                 Assert.AreEqual(a / b, _calculator.Div(a, b));
                 Assert.AreEqual(3.7737556561085972d, _calculator.Div(8.34, 2.21));
-    
+
                 Assert.True(Double.IsPositiveInfinity(_calculator.Div(8d, 0d)));
                 Assert.IsTrue(Double.IsNegativeInfinity(_calculator.Div(-8d, 0d)));
                 Assert.IsTrue(Double.IsNaN(_calculator.Div(0d, 0d)));
             });
-            
+
             Assert.AreSame(_calculator, calc);
 
             var iLoveDogs = true;
-            
+
             Assert.IsTrue(iLoveDogs);
 
             var myLoveToCats = 50;
             var myLoveToDogs = 100;
-            
+
             Assert.Greater(myLoveToDogs, myLoveToCats);
             //Assert.Pass();
         }
-        
+
         [OneTimeTearDown]
         public void OneTimeTearDown()
         {
             Console.WriteLine("Planned tests were run");
-        }
-    }
-
-    public class MyDataClass
-    {
-        public static IEnumerable TestCases
-        {
-            get
-            {
-                yield return new TestCaseData(12, 3).Returns(4);
-                yield return new TestCaseData(12, 2).Returns(6);
-                yield return new TestCaseData(12, 4).Returns(3);
-            }
         }
     }
 }
